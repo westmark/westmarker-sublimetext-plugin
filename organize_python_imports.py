c46ab07dac5786907e7246645bcd938ccc04a86b
Fredrik Westmark <fredrik.westmark@gmail.com>
2012-07-12 13:35:02 +0200

    Last commit introduced a new bug. Fixed.

--- organize_python_imports.py
+++ organize_python_imports.py
@@ -14,19 +14,29 @@ class OrganizePythonImportsCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         import_groups = []
-        code = '\n'.join([self.view.substr(line) for line in self.view.lines(sublime.Region(0, self.view.size())) if not self.view.substr(line).strip().startswith('#')])
+        code_lines = []
+        line_offset = 0
+
+        for line in self.view.lines(sublime.Region(0, self.view.size())):
+            line = self.view.substr(line)
+            if not line.strip().startswith('# -*- '):
+                code_lines.append(line)
+            else:
+                line_offset += 1
+
+        code = '\n'.join(code_lines)  # [self.view.substr(line) for line in self.view.lines(sublime.Region(0, self.view.size())) if not self.view.substr(line).strip().startswith('#')])
 
         st = ast.parse(code)
         current_group = None
 
         for stmt in st.body:
             if stmt.__class__ in [ast.Import, ast.ImportFrom]:
-                line_region = self.view.full_line(self.view.text_point(stmt.lineno - 1, 0))
+                line_region = self.view.full_line(self.view.text_point(stmt.lineno - 1 + line_offset, 0))
                 if not current_group:
                     current_group = ImportGroup(line_region.a, line_region.b)
                     import_groups.append(current_group)
                 current_group.imports.append(stmt)
-                current_group.end = line_region.b
+                current_group.end = max(line_region.b, current_group.end)
 
             else:
                 current_group = None
@@ -53,6 +63,6 @@ class OrganizePythonImportsCommand(sublime_plugin.TextCommand):
             import_str = '\n'.join(sorted(imports)) + '\n'
             import_str += '\n'.join(['from {0} import {1}'.format(p, ', '.join(sorted(from_imports.get(p)))) for p in sorted(from_imports.keys())])
 
-            self.view.replace(edit, sublime.Region(g.start, g.end), '\n' + import_str.strip('\n') + '\n')
+            self.view.replace(edit, sublime.Region(g.start, g.end), import_str.strip('\n') + '\n')
 
         self.view.end_edit(edit)
